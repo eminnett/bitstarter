530f95717bee46adee9525c7e73c0d6d0c047cf8
Edward Minnett <eminnett@gmail.com>
2013-07-08 23:25:46 +0100

    Coursera startup engineering course, exercise 3, part 1: changes to web.js

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+var fs = require('fs');
+
+app.get('/', function (request, response) {
+    fs.readFileSync('index.html', function (err, data) {
+        response.send(data);
+    });
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function () {
+    console.log("Listening on " + port);
 });
